0d8c198b844d72a6a6393cb9c87c19eeec84ccaa
Nimrod Flores <nimrodlflores@yahoo.com>
2019-06-09 17:13:12 +0800

    Applied changes needed to proceed with installation via bench v1.0

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+from pip._internal.req import parse_requirements
 
 version = "6.26.4"
 requirements = parse_requirements("requirements.txt", session="")
